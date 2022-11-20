44ef795f5042daf105c226135a17980d81c6ebf8
mmockelyn <trainznation@gmail.com>
2022-11-20 17:51:32 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
          * Package Service Providers...
          */
         Akibatech\FreeMobileSms\ServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
 
         /*
          * Application Service Providers...
