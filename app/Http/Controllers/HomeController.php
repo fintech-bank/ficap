4bee03e9aa1d5f1e400619981d51b4f9cb64a86a
mmockelyn <trainznation@gmail.com>
2022-11-21 20:15:33 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ class HomeController extends Controller
     public function __invoke()
     {
         $user = \Auth::user();
+        dd($user->onboarding()->percentageCompleted());
         if($user->created_at == $user->updated_at) {
             $user->update(['password' => null]);
             return redirect()->route('account')->with('info', "Veuillez changer votre mot de passe par default");
