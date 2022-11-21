645085cf87d048c4ca94e9ffbb22d81388e8bd6b
mmockelyn <trainznation@gmail.com>
2022-11-21 20:33:24 +0100

    Update index.blade.php

--- resources/views/caution/index.blade.php
+++ resources/views/caution/index.blade.php
@@ -22,6 +22,19 @@
                         <div class="fw-bold fs-4 mb-1">Crédit impacté</div>
                         <div class="">{{ $user->loan->wallet->name_account_generic }}</div>
                     </div>
+                    <div class="separator my-5"></div>
+                    <div class="d-flex flex-column">
+                        <div class="fw-bold fs-4 mb-1">Lié à la personne</div>
+                        <div class="d-flex flex-row">
+                            <div class="symbol symbol-30px">
+                                <img src="{{ Gravatar::get($user->loan->customer->user->email) }}" alt="">
+                            </div>
+                            <div class="d-flex flex-column">
+                                <div class="fw-bolder">{{ $user->loan->customer->info->full_name }}</div>
+                                <div class="">{{ $user->loan->customer->info->type_label }}</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
