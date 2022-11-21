49be5955bf5216f2029f5fda4929cbdf7cb1e936
mmockelyn <trainznation@gmail.com>
2022-11-21 20:27:38 +0100

    21

--- app/Http/Controllers/CautionController.php
+++ app/Http/Controllers/CautionController.php
@@ -6,6 +6,8 @@ class CautionController extends Controller
 {
     public function index()
     {
-
+        return view('caution.index', [
+            'user' => auth()->user()
+        ]);
     }
 }
