c1466fdc39775703966b13b9024f2c7e80f46785
mmockelyn <trainznation@gmail.com>
2022-11-21 19:16:37 +0100

    21

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -14,6 +14,8 @@ class LoginController extends Controller
     public function logout()
     {
         \Auth::logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
 
         return redirect()->back();
     }
