d3220f49e84499439443d5699ac1d1734744bfa2
mmockelyn <trainznation@gmail.com>
2022-11-21 19:39:28 +0100

    21

--- app/Http/Controllers/Account/AccountController.php
+++ app/Http/Controllers/Account/AccountController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
     public function index()
     {
         return view('account.index', ["user" => auth()->user()]);
+    }
+
+    public function password(Request $request)
+    {
+        auth()->user()->update([
+            'password' => \Hash::make($request->get('password'))
+        ]);
+
+        return redirect()->back()->with('success', "Le mot de passe à bien été définie");
     }
 }
--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -26,50 +26,57 @@
                     size="2"
                     color="primary" />
 
-                <!--begin::Main wrapper-->
-                <div class="fv-row" data-kt-password-meter="true">
-                    <!--begin::Wrapper-->
-                    <div class="mb-1">
-                        <!--begin::Label-->
-                        <label class="form-label fw-semibold fs-6 mb-2">
-                            Nouveau mot de passe
-                        </label>
-                        <!--end::Label-->
+                <form action="{{ route('account.password') }}" method="post">
+                    @csrf
+                    @method('PUT')
+                    <!--begin::Main wrapper-->
+                    <div class="fv-row" data-kt-password-meter="true">
+                        <!--begin::Wrapper-->
+                        <div class="mb-1">
+                            <!--begin::Label-->
+                            <label class="form-label fw-semibold fs-6 mb-2">
+                                Nouveau mot de passe
+                            </label>
+                            <!--end::Label-->
 
-                        <!--begin::Input wrapper-->
-                        <div class="position-relative mb-3">
-                            <input class="form-control form-control-lg form-control-solid"
-                                   type="password" placeholder="" name="password" autocomplete="off" />
+                            <!--begin::Input wrapper-->
+                            <div class="position-relative mb-3">
+                                <input class="form-control form-control-lg form-control-solid"
+                                       type="password" placeholder="" name="password" autocomplete="off" />
 
-                            <!--begin::Visibility toggle-->
-                            <span class="btn btn-sm btn-icon position-absolute translate-middle top-50 end-0 me-n2"
-                                  data-kt-password-meter-control="visibility">
+                                <!--begin::Visibility toggle-->
+                                <span class="btn btn-sm btn-icon position-absolute translate-middle top-50 end-0 me-n2"
+                                      data-kt-password-meter-control="visibility">
                                 <i class="bi bi-eye-slash fs-2"></i>
 
                                 <i class="bi bi-eye fs-2 d-none"></i>
                             </span>
-                            <!--end::Visibility toggle-->
-                        </div>
-                        <!--end::Input wrapper-->
+                                <!--end::Visibility toggle-->
+                            </div>
+                            <!--end::Input wrapper-->
 
-                        <!--begin::Highlight meter-->
-                        <div class="d-flex align-items-center mb-3" data-kt-password-meter-control="highlight">
-                            <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
-                            <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
-                            <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
-                            <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px"></div>
+                            <!--begin::Highlight meter-->
+                            <div class="d-flex align-items-center mb-3" data-kt-password-meter-control="highlight">
+                                <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
+                                <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
+                                <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px me-2"></div>
+                                <div class="flex-grow-1 bg-secondary bg-active-success rounded h-5px"></div>
+                            </div>
+                            <!--end::Highlight meter-->
                         </div>
-                        <!--end::Highlight meter-->
-                    </div>
-                    <!--end::Wrapper-->
+                        <!--end::Wrapper-->
 
-                    <!--begin::Hint-->
-                    <div class="text-muted">
-                        Use 8 or more characters with a mix of letters, numbers & symbols.
+                        <!--begin::Hint-->
+                        <div class="text-muted">
+                            Use 8 or more characters with a mix of letters, numbers & symbols.
+                        </div>
+                        <!--end::Hint-->
                     </div>
-                    <!--end::Hint-->
-                </div>
-                <!--end::Main wrapper-->
+                    <!--end::Main wrapper-->
+                    <div class="text-end">
+                        <x-form.button />
+                    </div>
+                </form>
             </div>
         </div>
     </div>
