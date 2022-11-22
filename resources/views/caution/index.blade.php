a6098effb101d6640838162edfef92fa63655eb7
mmockelyn <trainznation@gmail.com>
2022-11-22 02:01:06 +0100

    Update index.blade.php

--- resources/views/caution/index.blade.php
+++ resources/views/caution/index.blade.php
@@ -44,8 +44,10 @@
                 <div class="card-header">
                     <h3 class="card-title">Acte de cautionnement</h3>
                     <div class="card-toolbar">
-                        <button class="btn btn-sm btn-icon btn-secondary" id="go_previous"><i class="fa-solid fa-arrow-left"></i> </button>
-                        <button class="btn btn-sm btn-icon btn-secondary" id="go_next"><i class="fa-solid fa-arrow-right"></i> </button>
+                        <div class="btn-group">
+                            <button class="btn btn-sm btn-icon btn-secondary" id="go_previous"><i class="fa-solid fa-arrow-left"></i> </button>
+                            <button class="btn btn-sm btn-icon btn-secondary" id="go_next"><i class="fa-solid fa-arrow-right"></i> </button>
+                        </div>
                         <button type="button" class="btn btn-sm btn-success">
                             <i class="fa-solid fa-signature me-2"></i> Signer
                         </button>
