cff7a8ca4276b5ff77f40558b8019e8079f2552b
mmockelyn <trainznation@gmail.com>
2022-11-22 01:58:29 +0100

    Update index.blade.php

--- resources/views/caution/index.blade.php
+++ resources/views/caution/index.blade.php
@@ -101,5 +101,60 @@
                 });
             });
         }
+
+        document.getElementById('go_previous')
+            .addEventListener('click', (e) => {
+                if(myState.pdf == null|| myState.currentPage === 1)
+                    return;
+
+                myState.currentPage -= 1;
+                document.getElementById("current_page").value = myState.currentPage;
+                render();
+            });
+
+        document.getElementById('go_next')
+            .addEventListener('click', (e) => {
+                if(myState.pdf == null || myState.currentPage > myState.pdf._pdfInfo.numPages)
+                    return;
+
+                myState.currentPage += 1;
+                document.getElementById("current_page").value = myState.currentPage;
+                render();
+            });
+
+        document.getElementById('current_page')
+            .addEventListener('keypress', (e) => {
+                if(myState.pdf == null) return;
+
+                // Get key code
+                let code = (e.keyCode ? e.keyCode : e.which);
+
+                // If key code matches that of the Enter key
+                if(code === 13) {
+                    let desiredPage = document.getElementById('current_page').valueAsNumber;
+
+                    if(desiredPage >= 1 && desiredPage <= myState.pdf._pdfInfo.numPages) {
+                        myState.currentPage = desiredPage;
+                        document.getElementById("current_page").value = desiredPage;
+                        render();
+                    }
+                }
+            });
+
+        document.getElementById('zoom_in')
+            .addEventListener('click', (e) => {
+                if(myState.pdf == null) return;
+                myState.zoom += 0.5;
+
+                render();
+            });
+
+        document.getElementById('zoom_out')
+            .addEventListener('click', (e) => {
+                if(myState.pdf == null) return;
+                myState.zoom -= 0.5;
+
+                render();
+            });
     </script>
 @endsection
